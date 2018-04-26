1cd29a7dfdda238b6647067b244d58079c21742b
Gray Jackson <grayj@g.clemson.edu>
2018-04-26 00:13:09 -0400

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4459,9 +4459,9 @@ openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
-wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/mc1b0itxjnrvdhyzija6kxfqdbevdjii.vmdk
+wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/shared/static/5dukzod4ftj9v3g5r8q0ktxzweuj2vvw.vmdk
 wget -O /tmp/setup/OL7v2.vmdk https://clemson.box.com/shared/static/mc1b0itxjnrvdhyzija6kxfqdbevdjii.vmdk
-glance image-create --name OL7 --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7.vmdk
+glance image-create --name OL7 --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7v2.vmdk
 
 project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value z grep m1.small | cut -d' ' -f 1`
