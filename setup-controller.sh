f4c50a529f2a7d1119ff14c56bcd9f27ffb2ab4a
Gray Jackson <grayj@g.clemson.edu>
2018-04-29 10:50:34 -0400

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4464,7 +4464,7 @@ glance image-create --name OL7headnode --disk-format vmdk --visibility public --
 
 project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value z grep m1.small | cut -d' ' -f 1`
-image_id=`openstack image list -f value | grep OL7 | cut -d' ' -f 1`
+image_id=`openstack image list -f value | grep OL7headnode | cut -d' ' -f 1`
 security_id=`openstack security group list -f value | grep $project_id | cut -d' ' -f 1`
 port_id=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 
@@ -4472,7 +4472,7 @@ port_id=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 openstack server create --flavor m1.medium --security-group $security_id --image OL7headnode --nic port-id=$port_id headnode
 glance image-delete OL7headnode
 wget -O /tmp/setup/OL7compute https://clemson.box.com/shared/static/6t9golp60sxchjgvihrng013h2c5w0px.vmdk
-glance image-create --name OL7compute --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7
+glance image-create --name OL7compute --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7compute
 
 port_id=`openstack port list -f value | grep testport2 | cut -d' ' -f 1`
 openstack server create --flavor m1.medium --security-group $security_id --image OL7compute --nic port-id=$port_id compute1
