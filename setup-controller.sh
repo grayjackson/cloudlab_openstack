7413f0e6e259b7a32716bd6998f119390c441057
Gray Jackson <grayj@g.clemson.edu>
2018-04-29 22:46:09 -0400

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4454,13 +4454,15 @@ subnet_id1=`openstack network show -f shell flat-lan-1-net | grep "^subnets=" | 
 subnet_id2='openstack network show -f shell flat-lan-1-subnet | grep "^subnets=" | cut -d'"' -f 2'
 
 # See https://docs.openstack.org/python-openstackclient/pike/cli/command-objects/port.html
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id1},ip-address=10.11.10.21 testport1
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.22 testport2
-openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.23 testport3
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id1},ip-address=10.11.10.21 port1
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.22 port2
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.23 port3
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.24 port4
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.25 port5
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip-address=10.11.10.26 port6
+
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
-wget -O /tmp/setup/OL7compute.vmdk https://clemson.box.com/shared/static/6t9golp60sxchjgvihrng013h2c5w0px.vmdk
-glance image-create --name OL7compute --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7compute.vmdk
 
 project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value z grep m1.small | cut -d' ' -f 1`
@@ -4469,13 +4471,7 @@ security_id=`openstack security group list -f value | grep $project_id | cut -d'
 port_id=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 
 # See https://docs.openstack.org/mitaka/install-guide-ubuntu/launch-instance-selfservice.html
-openstack server create --flavor m1.medium --security-group $security_id --image OL7compute --nic port-id=$port_id headnode
 
-port_id=`openstack port list -f value | grep testport2 | cut -d' ' -f 1`
-openstack server create --flavor m1.medium --security-group $security_id --image OL7compute --nic port-id=$port_id compute1
-
-port_id=`openstack port list -f value | grep testport3 | cut -d' ' -f 1`
-openstack server create --flavor m1.medium --security-group $security_id --image OL7compute --nic port-id=$port_id compute2
 
 echo "***"
 echo "*** Done with OpenStack Setup!"
