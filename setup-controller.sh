081e0b905640befc230023a4d2e74e3bb645c801
Gray Jackson <grayj@g.clemson.edu>
2018-04-30 01:09:49 -0400

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4463,14 +4463,20 @@ openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id2},ip
 
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
+wget -O /tmp/setup/OL7head.vmdk https://clemson.box.com/shared/static/mc1b0itxjnrvdhyzija6kxfqdbevdjii.vmdk
+glance image-create --name OL7head --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7head.vmdk
+
 
 project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value z grep m1.small | cut -d' ' -f 1`
-image_id=`openstack image list -f value | grep OL7compute | cut -d' ' -f 1`
+image_id=`openstack image list -f value | grep OL7head | cut -d' ' -f 1`
 security_id=`openstack security group list -f value | grep $project_id | cut -d' ' -f 1`
-port_id=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
+port_id=`openstack port list -f value | grep port1 | cut -d' ' -f 1`
 
 # See https://docs.openstack.org/mitaka/install-guide-ubuntu/launch-instance-selfservice.html
+openstack server create --flavor m1.medium --security-group $security_id --image OL7compute --nic port-id=$port_id headnode
+
+
 
 
 echo "***"
